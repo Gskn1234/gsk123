080f72e8f8c63744637a0fe2feef38616123d09e
Gskn1234 <95078190+Gskn1234@users.noreply.github.com>
2021-11-29 19:09:35 +0530

    Create Array.c

---
+++ Array.c
@@ -0,0 +1,17 @@
+#include<stdioh>
+Element-[0],[0]:1
+Element-[0],[1]:2
+Element-[0],[2]:3
+Element-[1],[0]:4
+Element-[1],[1]:5
+Element-[1],[2]:6
+Element-[2],[0]:7
+Element-[2],[1]:8
+Element-[2],[2]:9
+    
+
+
+ The matrix is:
+1 2 3
+4 5 6
+7 8 9
