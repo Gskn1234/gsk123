d84c16d19b818813eba81393f638f2c6ef11c539
Gskn1234 <95078190+Gskn1234@users.noreply.github.com>
2021-11-29 18:47:04 +0530

    Pointer.c

---
+++ Pointer.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+   int marks[5]={89,90,87,86,76};//marks[0]=marks[4]
+   int i=0;
+   For(i=0,i=5,i++)
+   {
+   Printf("%d/t",marks[i]); 
+} 
+   }
+
+
+
+
+
+
+
+Output 1:
+ Enter marks 85%
+ You are passed
+ 
+
+
+Output 2: 
+ Enter marks 20%
+ You are failed
