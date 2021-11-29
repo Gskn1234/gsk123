a409beb8386ca1e19b83d19f980b30c222634a2c
Gskn1234 <95078190+Gskn1234@users.noreply.github.com>
2021-11-29 18:32:36 +0530

    Create LCM of 2 numbers.c

---
+++ LCM of 2 numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int LCM(int a,int b);
+int main()
+{
+int num1,num2,LCM;
+    Printf("enter any two numbers to find LCM:");
+    Scanf("%d%d",&num1&num2);
+    if(num1>num2)
+       LCM=LCM(num2,num1);
+    else;
+       LCM=lcm(num1,num2)
+    Printf("LCM of %d and %d=%d",num1,num2,lcm);
+    return 0;
+}
+int lcm(int a,int b)
+{
+Static int multiple=0;
+}
+
+
+
+
+
+Output:
+Enter any two numbers:8,9
+17 is an LCM of 2 number
